b7810515190fb7d51ecf9a0d4f1132fa06bd8002
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-03 23:04:07 -0300

    First commit

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+)
+
+type Actor struct {
+	Id   int
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+var actors []Actor
+
+func init() {
+	actors = append(actors, Actor{1, "Lucas", 29})
+	actors = append(actors, Actor{2, "Xiao", 30})
+}
+
+func NewActor(res http.ResponseWriter, req *http.Request) {
+	var actor Actor
+
+	//req.ParseForm()
+	//err := param.Parse(req.Form, &actor)
+	//if err != nil {
+	//http.Error(res, err.Error(), http.StatusBadRequest)
+	//	return
+	//}
+	err := json.NewDecoder(req.Body).Decode(&actor)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	actors = append(actors, actor)
+	url := fmt.Sprintf("/actors/%d", len(actors))
+	http.Redirect(res, req, url, http.StatusCreated)
+}
+
+func GetActors(res http.ResponseWriter, req *http.Request) {
+	data, err := json.Marshal(actors)
+	if err != nil {
+		log.Println("Error marshalling JSON")
+	}
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(data)
+}
+
+func GetActor(c web.C, res http.ResponseWriter, req *http.Request) {
+	var data []byte
+	id, err := strconv.Atoi(c.URLParams["id"])
+	if err != nil {
+		log.Fatal("Error converting to integer")
+	}
+	for _, v := range actors {
+		if v.Id == id {
+			data, err = json.Marshal(v)
+			res.Header().Set("Content-Type", "application/json")
+			res.Write(data)
+			break
+		}
+	}
+}
+
+func main() {
+	goji.Get("/", GetActors)
+	goji.Get("/actors/:id", GetActor)
+	goji.Post("/actors", NewActor)
+	//goji.Put("/actors/:id", UpdateActor)
+	//goji.Delete("/actors/:id", DeleteActor)
+	goji.Serve()
+}
