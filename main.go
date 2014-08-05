d393a2173f43328326a4f0e892adc0dff359a81c
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-04 22:15:07 -0300

    Add update handler

--- main.go
+++ main.go
@@ -26,13 +26,6 @@ func init() {
 
 func NewActor(res http.ResponseWriter, req *http.Request) {
 	var actor Actor
-
-	//req.ParseForm()
-	//err := param.Parse(req.Form, &actor)
-	//if err != nil {
-	//http.Error(res, err.Error(), http.StatusBadRequest)
-	//	return
-	//}
 	err := json.NewDecoder(req.Body).Decode(&actor)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusBadRequest)
@@ -68,11 +61,24 @@ func GetActor(c web.C, res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func UpdateActor(c web.C, res http.ResponseWriter, req *http.Request) {
+	var actor Actor
+	id, err := strconv.Atoi(c.URLParams["id"])
+	if err != nil {
+		log.Fatal("Error converting to integer")
+	}
+	err := json.NewDecoder(req.Body).Decode(&actor)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	actors[i-1] = actor
+}
+
 func main() {
 	goji.Get("/", GetActors)
 	goji.Get("/actors/:id", GetActor)
 	goji.Post("/actors", NewActor)
-	//goji.Put("/actors/:id", UpdateActor)
-	//goji.Delete("/actors/:id", DeleteActor)
+	goji.Put("/actors/:id", UpdateActor)
 	goji.Serve()
 }
